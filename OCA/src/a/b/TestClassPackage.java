0a164a10cf790666c4def783ff1b0639cf0bb366
Cif2018 <39955445+Cif2018@users.noreply.github.com>
2020-10-01 07:03:27 +0300

    01.10.2020 morning

--- OCA/src/a/b/TestClassPackage.java
+++ OCA/src/a/b/TestClassPackage.java
@@ -1,10 +1,28 @@
 package a.b;
 
+import static java.lang.System.out;
+
 public class TestClassPackage {
+
+	public static int suma(int a, int b) {
+		int x;
+		return a + b;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		TestClassPackage tcp = new TestClassPackage();
+		out.println("inside main");
+		int i = 10_00;
+		out.println(i + 1);
+		out.println(suma(1, 3));
+	}
 
+	{
+		out.println("bloc initializat la instantiere...1");
+	}
+	{
+		out.println("bloc initializat la instantiere...2");
 	}
 
 }
