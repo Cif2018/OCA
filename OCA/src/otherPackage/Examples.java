73ceb42b86b749673642546b584e17d5f7cf07e0
Cif2018 <39955445+Cif2018@users.noreply.github.com>
2020-10-03 18:29:27 +0300

    .

---
+++ OCA/src/otherPackage/Examples.java
@@ -0,0 +1,33 @@
+package otherPackage;
+
+public class Examples {
+	int k;
+	double d=1_2.0;
+	public Examples(){
+		this.k=this.k+1;
+	}
+	Integer ii=15;
+	
+	{int i=0xE;
+//	i=i+1;
+	System.out.println(i);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Integer.toHexString(17).toUpperCase());
+		Examples e=new Examples();	
+		
+		System.out.println(e.k);
+System.out.println("done!");
+	}
+	
+	public int metoda() { 
+	Object o = null;
+	System.out.println(o.toString());
+		int i;
+//		i=i+1;
+		return 1_1;
+	}
+
+}
