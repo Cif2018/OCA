5c11bced085a4be8339ea2789a96866303593970
Cif2018 <39955445+Cif2018@users.noreply.github.com>
2020-10-17 14:20:10 +0300

    x

--- OCA/src/Ch2/Test4.java
+++ OCA/src/Ch2/Test4.java
@@ -1,26 +1,49 @@
 package Ch2;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import Ch2.Test;
 
 public class Test4 {
 	static String food = "milk";
+	int i = 0;
 
 	public static void main(String[] args) {
-//		int i=0;
-		for (int i=0,j=0;i<10;i++,j++){
+		
+		List<String> values = new ArrayList<String>();
+		
+		String[] AS1=new String[2]; //OBS: there is no compile err, two null values would return instead
+		for (String s:AS1) {
+			System.out.println(s);
+		}
+		
+		String[] AS= {"x1","x2","x3"};
+//		String s; wrong!
+		for (String s:AS) {
+			System.out.println(s);
+		}
+				
+		int i;
+		int j;
+		for (i = 0, j = 0; i < 10; i++, j++) {
 			System.out.print(i);
 			System.out.println(j);
 		}
-		
-		someFood someFoodVar =someFood.BREAD;
+
+//		for (int i = 0, j = 0; i < 10; i++, j++) {
+//			System.out.print(i);
+//			System.out.println(j);
+//		}
+
+		someFood someFoodVar = someFood.BREAD;
 		switch (someFoodVar) {
 		case BREAD:
 			System.out.println("enum BREAD here");
 			break;
 		}
-		
+
 		System.out.println(someFood.valueOf("MILK"));
 
 //		final String milk = "milk";
@@ -33,8 +56,7 @@ public class Test4 {
 //		default:
 //			System.out.println("other");
 //		}
-		
-		
+
 //		String s;
 //		System.out.println(s=((1>2)?"T":"F"));
 //		boolean b=false;
@@ -55,11 +77,9 @@ public class Test4 {
 //		System.out.println(x.equals(y));
 
 	}
-	
-	public enum someFood{
-		BREAD,MILK,WATER
-	}
-	
 
+	public enum someFood {
+		BREAD, MILK, WATER
+	}
 
 }
